581ae17955df5eb4a1848caa5aec5ffc70b00f64
Ahmed Arefin <ahmedarefinsajjad@gmail.com>
2023-09-11 06:50:16 +0600

    update

--- clone.sh
+++ clone.sh
@@ -8,7 +8,7 @@ echo -e "$green << cloned kernel successfully >> \n $white"
 # Tool Chain
 echo -e "$green << cloning gcc >> \n $white"
 git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9 "$HOME"/gcc64
-git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9"$HOME"/gcc32
+git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9 "$HOME"/gcc32
 echo -e "$green << cloned gcc successfully >> \n $white"
 
 # Clang
